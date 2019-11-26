7e488489ad4d636d1b6fcde3d83a6b55de4ed044
k1-k0 <kmssrvk@gmail.com>
2019-11-26 08:43:05 +0300

    Add extra approach for last task of 6th chapter

--- chapter_06/borrow.js
+++ chapter_06/borrow.js
@@ -1,7 +1,13 @@
 let map = {one: true, two: true, hasOwnProperty: true};
+
+// 1st approach:
 map[Symbol.hasOwnProperty] = function() {
     return this.hasOwnProperty;
 }
+
+// 2nd approach:
+// Object.prototype.hasOwnProperty(map, () => this.hasOwnProperty);
+
 
 console.log(map.hasOwnProperty);
 // → true
